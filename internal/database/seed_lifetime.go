75216d4dd9fbd47fd2051b03bf0469286040601f
MichaelTinney <michaeltinney@pm.me>
2025-11-17 15:04:58 -0500

    Create seed_lifetime.go

---
+++ internal/database/seed_lifetime.go
@@ -0,0 +1,19 @@
+package database
+
+import "zapmanejo-backend2/internal/models"
+
+func SeedLifetimeSlots() {
+	var count int64
+	DB.Model(&models.LifetimeSlot{}).Count(&count)
+	if count > 0 {
+		return // already seeded
+	}
+
+	for i := 1; i <= 200; i++ {
+		slot := models.LifetimeSlot{
+			Slot:     i,
+			Occupied: false,
+		}
+		DB.Create(&slot)
+	}
+}
