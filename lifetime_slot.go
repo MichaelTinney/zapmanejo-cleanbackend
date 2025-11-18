cafce6fa60222ab9a119dfc8112bc4a23a771fff
MichaelTinney <michaeltinney@pm.me>
2025-11-18 15:17:39 -0500

    Delete lifetime_slot.go

--- lifetime_slot.go
+++
@@ -1,9 +0,0 @@
-// internal/models/lifetime_slot.go
-package models
-
-type LifetimeSlot struct {
-	ID        uint `gorm:"primaryKey"`
-	Slot      int  `gorm:"unique;not null"`
-	UserID    *uint
-	Occupied  bool `gorm:"default:false"`
-}
